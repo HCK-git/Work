7a5f0973a9dc43ee1c5362655de6a418b4ba4ef9
Kirill <matanoff.kirill@yandex.ru>
2021-04-07 00:11:32 +0300

    Reading CSV

---
+++ Main.py
@@ -0,0 +1,16 @@
+import urllib.request
+import csv
+
+destination = 'Data'
+url = 'https://www.moex.com/ru/listing/securities-list-csv.aspx?type=1'
+# urllib.request.urlretrieve(url, destination)
+
+with open("Data", encoding="windows-1251") as f:
+    file_reader = csv.reader(f, delimiter=',')
+    count = 0
+    for row in file_reader:
+        if count == 0:
+            print("Файл имеет столбцы {}".format(", ".join(row)))
+        else:
+            print(row)
+        count += 1
