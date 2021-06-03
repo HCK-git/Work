827bf8fd1f93cbc9451067506c57a11f9f9c7495
Vladimir <newvovabottom@gmail.com>
2021-06-03 19:33:56 +0300

    Comlete save

--- AnalizData.py
+++ AnalizData.py
@@ -7,36 +7,69 @@ import itertools
 import multiprocessing
 import os
 import json
+import urllib
+import io
+import zipfile
+import tempfile
 
-df = pd.read_csv('Data', encoding='1251')
-pd.set_option('display.max_columns', None)
-df = df[['EMITENT_FULL_NAME', 'DISCLOSURE_RF_INFO_PAGE']].drop_duplicates()
-# print(df)
-
-df['DISCLOSURE_RF_INFO_PAGE'].fillna(0, inplace=True)
-inform_dict = df.groupby(['EMITENT_FULL_NAME'])['DISCLOSURE_RF_INFO_PAGE'].apply(list).to_dict()
-pprint(inform_dict)
-url_list = []
-for elem in inform_dict.keys():
-    if inform_dict[elem][0] != 0 and "e-disclosure" in inform_dict[elem][0]:
-        url_list.append(inform_dict[elem])
+# df = pd.read_csv('Data', encoding='1251')
+# pd.set_option('display.max_columns', None)
+# df = df[['EMITENT_FULL_NAME', 'DISCLOSURE_RF_INFO_PAGE']].drop_duplicates()
+# # print(df)
+#
+# df['DISCLOSURE_RF_INFO_PAGE'].fillna(0, inplace=True)
+# inform_dict = df.groupby(['EMITENT_FULL_NAME'])['DISCLOSURE_RF_INFO_PAGE'].apply(list).to_dict()
+# pprint(inform_dict)
+# url_dict = {}
+# for elem in inform_dict.keys():
+#     if inform_dict[elem][0] != 0 and "e-disclosure" in inform_dict[elem][0]:
+#         url_dict[elem] = inform_dict[elem]
 # pprint(url_list)
+
+
+def saving():
+    # for elem in url.keys():
+    #     if "e-disclosure" in url[elem]:
+            link = 'https://e-disclosure.ru/portal/files.aspx?id=2798&type=2'
+    # url[elem]
+            response = requests.get(link)
+            soup = BeautifulSoup(response.text, 'lxml')
+            # quotes = soup.find_all('a')
+            quotes = soup.findAll("a", {"class": "file-link"})
+            print(quotes)
+            for elements in quotes:
+                string_united = ''
+                # print('4')
+                string = str(elements)
+                # print('5')
+                for i in string:
+                    string_united = string_united + i
+                beg_url = string_united.find('href="http')+6
+                end_url = string_united.find('">')
+                print(string_united[beg_url:end_url])
+            response = requests.get('https://e-disclosure.ru/portal/FileLoad.ashx?Fileid=1356012')
+            file = tempfile.TemporaryFile()
+            file.write(response.content)
+            fzip = zipfile.ZipFile(file)
+            fzip.extractall('Data/reporting/')
+            file.close()
+            fzip.close()
 
 
 def url_callback(urls):
     # pprint(inform_dict)
-    listik = []
+    dict_otch = {}
     k = 0
-    for elem in urls:
+    for elem in urls.keys():
 
         print(elem)
         # print('1')
-        if " " in elem:
-            elem = elem.replace(" ", '')
-        if "https:" not in elem and "http:" not in elem:
-            elem = "https://" + elem
+        if " " in urls[elem]:
+            urls[elem] = urls[elem].replace(" ", '')
+        if "https:" not in urls[elem] and "http:" not in urls[elem]:
+            urls[elem] = "https://" + urls[elem]
             # print('2')
-        url = elem
+        url = urls[elem]
         response = requests.get(url)
         print('404')
         soup = BeautifulSoup(response.text, 'lxml')
@@ -58,10 +91,10 @@ def url_callback(urls):
                 string_united = string_united.replace(string_united[first_amp:second_amp+1], '')
                 first = string_united.find('"')
                 second = string_united.find('">')
-                # print("https://e-disclosure.ru/"+string_united[first+1:second])
+                print("https://e-disclosure.ru/"+string_united[first+1:second])
+                dict_otch = "https://e-disclosure.ru/"+string_united[first+1:second]
                 # print('7')
-                listik.append("https://e-disclosure.ru/"+string_united[first+1:second])
-    return listik
+    return dict_otch
 
 
 def make_file(data):
@@ -69,15 +102,23 @@ def make_file(data):
         # for elem in urls_cleaned.keys():
         json.dump(data, f)
 
-if __name__ == '__main__':
-    if os.path.exists("List.txt"):
-        pass
-    else
-        multiprocessing.freeze_support()
-        mp = multiprocessing.Pool(processes=8)
-        urls_with_reports = mp.imap_unordered(url_callback, url_list)
-        urls_cleaned = dict(itertools.chain(*urls_with_reports))
-        make_file(urls_cleaned)
-    # print(urls_cleaned)
+
+def read_file():
+    with open("List.json", "r") as f:
+        list_info = json.load(f)
+    return list_info
 
 
+
+saving()
+
+# if __name__ == '__main__':
+#     if os.path.exists("List.json"):
+#         urls_cleaned = read_file()
+#     else:
+#         multiprocessing.freeze_support()
+#         mp = multiprocessing.Pool(processes=8)
+#         urls_with_reports = mp.imap_unordered(url_callback, url_dict)
+#         urls_cleaned = dict(itertools.chain(*urls_with_reports))
+#         make_file(urls_cleaned)
+#     # print(urls_cleaned)
